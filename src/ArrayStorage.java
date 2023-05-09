56e9d8cac5af04c504d931d30c3e88faaa48226a
Ilya Yakovlev <93yakovlev0204@mail.ru>
2023-05-09 13:32:02 +0300

    Clear and Get methods are fixed

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,74 +5,58 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int count=0; //Counter for added resume
+    private int count;
 
     void clear() {
-        // Replace all elements by null
-        Arrays.fill(storage, null);
-        // Drop the counter
-        count=0;
+        Arrays.fill(storage, 0, count, null);
+        count = 0;
     }
 
     void save(Resume r) {
-        // Save element to next empty slot
         storage[count] = r;
-        // Increase amount of array elements
         count++;
     }
 
     Resume get(String uuid) {
-        // Check every resume uuid
-        for (Resume r : storage)
-        {
-            try{
-                // Return resume if uuid of element matched with requested uuid
-                if (r.uuid.equals(uuid)) {
-                return r;
-                }
-            } // Catch NullPointerException and ignore it
-            catch (NullPointerException ignored){}
+        for (int i = 0; i < count; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
         }
-        // Return null if there are no matches
         return null;
     }
 
-
     void delete(String uuid) {
-        for(int i=0; i<storage.length; i++){
-            // Added counter for checking element position in array
+        for (int i = 0; i < count; i++) {
             Resume innerCounter = storage[i];
-            // If uuid of element and required uuid are the same -> delete element
-            if(innerCounter.uuid.equals(uuid)){
-                storage[i]=null;
-                // Reduce counter of exists resumes
+            if (innerCounter.uuid.equals(uuid)) {
+                storage[i] = null;
                 count--;
                 // If there are elements after deleted -> moving elements left
-                while (storage[i+1]!=null){
-                    storage[i] = storage[i+1];
-                    storage[i+1] = null;
+                while (storage[i + 1] != null) {
+                    storage[i] = storage[i + 1];
+                    storage[i + 1] = null;
                     i++;
                 }
                 return;
             }
-        } System.out.println("Данный uuid отсутствует в массиве!");
+        }
+        System.out.println("Данный uuid отсутствует в массиве!");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        // Create an array for exact count of elements
         Resume[] resume = new Resume[count];
         // Add "Not null" elements to new array
-        for(int i=0; i<resume.length; i++){
+        for (int i = 0; i < count; i++) {
             resume[i] = storage[i];
         }
         return resume;
     }
 
     int size() {
-        // Return amount of exists resumes
         return count;
     }
 }
