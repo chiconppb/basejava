a915c064f09b3caf9c46b53a2208e1768ced8031
Ilya Yakovlev <93yakovlev0204@mail.ru>
2023-05-18 17:37:41 +0300

    Revert "Rebuild save method, excluded duplicated search in get, update, delete"
    This reverts commit a71ec8d743111ab77061b23fc6b75ba41e5a4131.

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -8,48 +8,46 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private final int STORAGE_LIMIT = 10000;
-    private final Resume[] storage = new Resume[STORAGE_LIMIT];
+    private Resume[] storage = new Resume[10000];
     private int count;
 
-    public void clear() {
+    void clear() {
         Arrays.fill(storage, 0, count, null);
         count = 0;
     }
 
-    public void save(Resume r) {
-        if (count >= STORAGE_LIMIT) {
-            System.out.println("ERROR: Массив полон!");
-        } else if (getIndex(r.getUuid()) < 0) {
+    void save(Resume r) {
+        if (count < storage.length) {
+            if (this.search(r.getUuid()) < 0) {
+                storage[count] = r;
+                count++;
+                return;
+            }
             System.out.println("ERROR: " + r.getUuid() + " уже существует!");
-        } else {
-            storage[count] = r;
-            count++;
+            return;
         }
+        System.out.println("ERROR: Массив полон!");
     }
 
-    public Resume get(String uuid) {
-        int index = getIndex(uuid);
-        if (index >= 0) {
-            return storage[index];
+    Resume get(String uuid) {
+        if (this.search(uuid) >= 0) {
+            return storage[this.search(uuid)];
         }
         System.out.println("ERROR: " + uuid + " отсутствует в массиве!");
         return null;
     }
 
     public void update(Resume resume) {
-        int index = getIndex(resume.getUuid());
-        if (index >= 0) {
-            storage[index] = resume;
+        if (this.search(resume.getUuid()) >= 0) {
+            storage[this.search(resume.getUuid())] = resume;
             return;
         }
         System.out.println("ERROR: " + resume.getUuid() + " отсутствует в массиве!");
     }
 
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index >= 0) {
-            storage[index] = storage[count - 1];
+    void delete(String uuid) {
+        if (this.search(uuid) >= 0) {
+            storage[this.search(uuid)] = storage[count - 1];
             storage[count - 1] = null;
             count--;
             return;
@@ -60,15 +58,15 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    public Resume[] getAll() {
+    Resume[] getAll() {
         return Arrays.copyOf(storage, count);
     }
 
-    public int size() {
+    int size() {
         return count;
     }
 
-    private int getIndex(String uuid) {
+    private int search(String uuid) {
         for (int i = 0; i < count; i++) {
             if (uuid.equals(storage[i].getUuid())) {
                 return i;
