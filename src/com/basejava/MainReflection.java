45af21dc39e4ee83538dc20a5515695916fa4f93
Ilya Yakovlev <93yakovlev0204@mail.ru>
2023-06-05 16:53:42 +0300

    Add toString method invoke via reflection

--- src/com/basejava/MainReflection.java
+++ src/com/basejava/MainReflection.java
@@ -3,16 +3,20 @@ package com.basejava;
 import com.basejava.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
         Resume r = new Resume();
-        Field field = r.getClass().getDeclaredField()[0];
+        Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
-        //TODO : invoke r.stoString via reflection
         System.out.println(r);
+        System.out.println("=================================");
+        Method m1 = r.getClass().getMethod("toString");
+        System.out.println(m1.getName() +" via reflection: " + m1.invoke(r));
     }
 }
