43daa268b64d54ffeefcb1638cbefee623a12ebb
Ilya Yakovlev <93yakovlev0204@mail.ru>
2023-07-08 22:02:33 +0300

    Logging implementation

--- src/com/basejava/webapp/storage/AbstractStorage.java
+++ src/com/basejava/webapp/storage/AbstractStorage.java
@@ -6,33 +6,41 @@ import com.basejava.webapp.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<T> implements Storage {
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         return doGet(getExistingSearchKey(uuid));
     }
 
     public List<Resume> getAllSorted() {
+        LOG.info("Get all sorted");
         List<Resume> allResumes = doCopyAll();
         Collections.sort(allResumes);
         return allResumes;
     }
 
     public void save(Resume resume) {
+        LOG.info("Save " + resume);
         doSave(getNotExistingSearchKey(resume.getUuid()), resume);
     }
 
     public void update(Resume resume) {
+        LOG.info("Update " + resume);
         doUpdate(getExistingSearchKey(resume.getUuid()), resume);
     }
 
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         doDelete(getExistingSearchKey(uuid));
     }
 
     protected T getExistingSearchKey(String uuid) {
         if (!isExist(getSearchKey(uuid))) {
+            LOG.warning("Resume " + uuid + " is not exist!");
             throw new NotExistStorageException(uuid);
         }
         return getSearchKey(uuid);
@@ -40,6 +48,7 @@ public abstract class AbstractStorage<T> implements Storage {
 
     protected T getNotExistingSearchKey(String uuid) {
         if (isExist(getSearchKey(uuid))) {
+            LOG.warning("Resume " + uuid + " is already exist!");
             throw new ExistStorageException(uuid);
         }
         return getSearchKey(uuid);
