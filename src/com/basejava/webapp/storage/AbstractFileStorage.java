f1995886f69cd137027918f0d2d46a51ae166171
Ilya Yakovlev <93yakovlev0204@mail.ru>
2023-07-28 23:13:27 +0300

    HW8 AbstractFileStorage implementation

---
+++ src/com/basejava/webapp/storage/AbstractFileStorage.java
@@ -0,0 +1,106 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.exception.StorageException;
+import com.basejava.webapp.model.Resume;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public abstract class AbstractFileStorage extends AbstractStorage<File> {
+    private final File directory;
+
+    protected AbstractFileStorage(File directory) {
+        Objects.requireNonNull(directory, "directory must not be null");
+        if (!directory.isDirectory()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + "is not directory");
+        }
+        if (!directory.canRead() || !directory.canWrite()) {
+            throw new IllegalArgumentException(directory.getAbsolutePath() + "is not readable/writable");
+        }
+        this.directory = directory;
+    }
+
+    @Override
+    protected File getSearchKey(String uuid) {
+        return new File(directory, uuid);
+    }
+
+    @Override
+    protected boolean isExist(File file) {
+        return file.exists();
+    }
+
+    @Override
+    protected Resume doGet(File file) {
+        try {
+            return doRead(file);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+    }
+
+    protected abstract Resume doRead(File file) throws IOException;
+
+    @Override
+    protected void doSave(File file, Resume r) {
+        try {
+            file.createNewFile();
+            doWrite(r, file);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+    }
+
+    protected abstract void doWrite(Resume r, File file) throws IOException;
+
+    @Override
+    protected void doUpdate(File file, Resume r) {
+        try {
+            doWrite(r, file);
+        } catch (IOException e) {
+            throw new StorageException("IO error", file.getName(), e);
+        }
+
+    }
+
+    @Override
+    protected void doDelete(File file) {
+        file.delete();
+    }
+
+    @Override
+    protected List<Resume> doCopyAll(){
+        List<Resume> allResumes = new ArrayList<>();
+        File[] files = directory.listFiles();
+        Objects.requireNonNull(files);
+        for (File f : files) {
+            try {
+                allResumes.add(doRead(f));
+            } catch (IOException e) {
+                throw new RuntimeException("The file can't be added",e);
+            }
+        }
+        return allResumes;
+    }
+
+    @Override
+    public void clear() {
+        File[] d = directory.listFiles();
+        if (d.length == 0) {
+            return;
+        }
+        for (int i = 0; i < size(); i++) {
+            d[i].delete();
+        }
+    }
+
+    @Override
+    public int size() {
+        File[] dir = directory.listFiles();
+        Objects.requireNonNull(dir);
+        return dir.length;
+    }
+}
