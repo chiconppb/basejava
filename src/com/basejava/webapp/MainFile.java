909b8765069829a9d664a7cb178fe749b2fdc8e5
Ilya Yakovlev <93yakovlev0204@mail.ru>
2023-08-18 14:30:04 +0300

    HW9 Add offsets to recursive call to MainFile

--- src/com/basejava/webapp/MainFile.java
+++ src/com/basejava/webapp/MainFile.java
@@ -7,7 +7,7 @@ public class MainFile {
     public static void main(String[] args) throws IOException {
         File file = new File("./.gitignore");
         System.out.println(file.getCanonicalPath());
-        File dir = new File(".\\src\\com\\basejava\\webapp");
+        File dir = new File(".\\src");
         System.out.println(dir.isDirectory());
         String[] list = dir.list();
         if (list != null) {
@@ -19,18 +19,23 @@ public class MainFile {
         printDeepDirectory(dir);
     }
 
+    public static StringBuffer offset = new StringBuffer();
+
     private static void printDeepDirectory(File dir) {
         File[] files = dir.listFiles();
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
-                    System.out.println("File: " + file.getName());
+                    System.out.println(offset + "->File: " + file.getName());
                 } else if (file.isDirectory()) {
-                    System.out.println("Directory: " + file.getName());
+                    System.out.println(offset + "*Directory: " + file.getName());
+                    offset.append("\t");
                     printDeepDirectory(file);
+                    offset.deleteCharAt(offset.length() - 1);
                 }
             }
         }
     }
 }
+
 
