d0d9ba05f85bcc544c4e16f40e0e3e4f8694065f
Ilya Yakovlev <93yakovlev0204@mail.ru>
2023-10-24 13:28:31 +0300

    HW12 refactor oddOrEven method in the single stream

--- src/com/basejava/webapp/StreamTask.java
+++ src/com/basejava/webapp/StreamTask.java
@@ -15,31 +15,48 @@ public class StreamTask {
 
     static Random random = new Random();
 
-    int minValue(int[] values) {
+    private int minValue(int[] values) {
         return Arrays.stream(values)
                 .distinct()
                 .sorted()
                 .reduce(0, (acc, val) -> (acc * 10) + val);
     }
 
-    List<Integer> oddOrEven(List<Integer> integers) {
-        int streamSum = integers.stream()
-                .mapToInt(Integer::intValue)
-                .sum();
+    private List<Integer> oddOrEven(List<Integer> integers) {
+        System.out.println("Sum is " + integers.stream().mapToInt(Integer::intValue).sum());
 
-        List<Integer> map = integers.stream()
-                .collect(Collectors
-                        .partitioningBy(number -> (number % 2) == 0))
-                .values()
-                .stream()
-                .toList()
-                .get(((streamSum % 2) == 0) ? 0 : 1);
-
-        System.out.println("Sum is " + streamSum);
-        return map;
+        final Map<Boolean, List<Integer>> map = integers.stream()
+                .collect(Collectors.partitioningBy(number -> ((number % 2) == 0)));
+        return map.get(map.get(false).size() % 2 != 0);
+///////////////////////////////////////////////////////////////////////////////////
+//        final Map<Boolean, List<Integer>> map = integers
+//                .stream()
+//                .collect(Collectors.partitioningBy(number -> (number % 2) == 0));
+//
+//        boolean isOdd = integers
+//                .stream()
+//                .mapToInt(Integer::intValue)
+//                .sum() % 2 != 0;
+//
+//        return map.get(!isOdd);
+///////////////////////////////////////////////////////////////////////////////////
+//        int streamSum = integers.stream()
+//                .mapToInt(Integer::intValue)
+//                .sum();
+//
+//        List<Integer> map = integers.stream()
+//                .collect(Collectors
+//                        .partitioningBy(number -> (number % 2) == 0))
+//                .values()
+//                .stream()
+//                .toList()
+//                .get(((streamSum % 2) == 0) ? 0 : 1);
+//
+//        System.out.println("Sum is " + streamSum);
+//        return map;
     }
 
-    int[] getRandomNumbers(int count) {
+    private int[] getRandomNumbers(int count) {
         int[] numbers = new int[count];
         for (int i = 0; i < count; i++) {
             int number = random.nextInt(9) + 1;
@@ -49,7 +66,7 @@ public class StreamTask {
         return numbers;
     }
 
-    List<Integer> getRandomList(int count) {
+    private List<Integer> getRandomList(int count) {
         List<Integer> list = new ArrayList<>(count);
         for (int i = 0; i < count; i++) {
             list.add(random.nextInt(count));
