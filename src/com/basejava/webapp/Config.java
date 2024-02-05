c8cac81361df8735abd1fc82eeeacce805dd923a
Ilya Yakovlev <93yakovlev0204@mail.ru>
2024-02-05 09:43:30 +0300

    HW15 Add resumes html table in doGet

--- src/com/basejava/webapp/Config.java
+++ src/com/basejava/webapp/Config.java
@@ -8,7 +8,8 @@ import java.util.Properties;
 
 public class Config {
     private static final Config INSTANCE = new Config();
-    protected final File PROPS = new File(".\\config\\resumes.properties");
+//    protected final File PROPS = new File(".\\config\\resumes.properties");
+    protected final File PROPS = new File("C:\\Users\\CHICO\\Desktop\\lessons\\basejava\\config\\resumes.properties");
     private final File storageDir;
     private final Storage storage;
 
@@ -22,7 +23,6 @@ public class Config {
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
-
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config file" + PROPS.getAbsolutePath());
         }
--- src/com/basejava/webapp/web/ResumeServlet.java
+++ src/com/basejava/webapp/web/ResumeServlet.java
@@ -1,23 +1,70 @@
 package com.basejava.webapp.web;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
+import com.basejava.webapp.Config;
+import com.basejava.webapp.model.Resume;
+import com.basejava.webapp.storage.Storage;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Storage storage = Config.getInstance().getStorage();
+        List<Resume> resumes = storage.getAllSorted();
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
 //        response.setHeader("Content-Type", "text/html; charset=UTF-8");
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
-
+//        String name = request.getParameter("name");
+//        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<style>");
+        out.println("""
+                table {
+                  font-family: arial, sans-serif;
+                  border-collapse: collapse;
+                  width: 50%;
+                }""");
+        out.println("""
+                td, th {
+                  border: 1px solid #dddddd;
+                  text-align: left;
+                  padding: 8px;
+                }""");
+        out.println("""
+                tr:nth-child(even) {
+                  background-color: #dddddd;
+                }""");
+        out.println("</style>");
+        out.println("<body>");
+        out.println("<table>");
+        printRow(out, "uuid", "fullname");
+        for (Resume r : resumes) {
+            printRow(out, r.getUuid(), r.getFullName());
+        }
+        out.println("</table>");
+        out.println("</body>");
+        out.println("/<html>");
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 
+    }
+
+    private void printRow(PrintWriter out, String... strings) {
+        out.println("<tr>");
+        for (String s : strings) {
+            out.println("<td>");
+            out.println(s);
+            out.println("</td>");
+        }
+        out.println("</tr>");
     }
 }
